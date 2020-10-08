203aa10e143d2b43dc058c4698b7dd7708243637
Alan Torres <70346874+alantorres729@users.noreply.github.com>
2020-10-07 23:08:45 -0500

    CSS changes to Sign In page.

--- public/scripts/signIn.js
+++ public/scripts/signIn.js
@@ -1,3 +1,5 @@
+import * as Helper from "../../src/controllers/commands/helpers/helper";
+
 document.addEventListener("DOMContentLoaded", () => {
 	// TODO: Anything you want to do when the page is loaded?
 });
@@ -5,17 +7,17 @@ document.addEventListener("DOMContentLoaded", () => {
 function validateForm() {
 	var employeeID = document.getElementById("employeeID").value;
 	var password = document.getElementById("password").value;
-	if(employeeID != '' && Number.isInteger(employeeID) && password != ''){
+	if(Helper.isBlankString(employeeID.toString()) && Number.isInteger(employeeID) && Helper.isBlankString(password)){
 		return true;
 	} else {
-		if(employeeID == ''){
+		if(Helper.isBlankString(employeeID)){
 			document.getElementById("errorTest").innerHTML = "* Employee ID field empty.";
 		}
-		if(password == ''){
+		else if(Helper.isBlankString(password)){
 			document.getElementById("errorTest").innerHTML = "* Password field empty.";
 		}
-		if(!Number.isInteger(employeeID)){
-			document.getElementById("errorTest").innerHTML = "* Employee ID entry not a number.";
+		else if(!Number.isInteger(employeeID)){
+			document.getElementById("errorTest").innerHTML = "* Employee ID entry not a valid number.";
 		}
 		return false;
 	}
