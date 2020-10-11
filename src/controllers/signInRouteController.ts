62b7abe976ef4db86e39667086d207ffb72cbc88
cmsturge <codysturgeon8@gmail.com>
2020-10-10 19:35:17 -0500

    Didnt have this file in previous commit

--- src/controllers/signInRouteController.ts
+++ src/controllers/signInRouteController.ts
@@ -1,38 +1,50 @@
 import { Request, Response } from "express";
-import * as ActiveEmployeeExistsQuery from "..commands/employees/activeEmployeeExistsQuery";
+import * as ActiveEmployeeExistsQuery from "./commands/employees/activeEmployeeExistsQuery";
 import { Resources } from "../resourceLookup";
 import * as Helper from "./helpers/routeControllerHelper";
-import { ViewNameLookup, QueryParameterLookup } from "./lookups/routingLookup";
-import { PageResponse, CommandResponse, ActiveUser, MainMenuPageResponse } from "./typeDefinitions";
-import * as ActiveUserModel from "..commands/models/activeUserModel";
-import * as EmployeeModel from "..commands/models/employeeModel";
-import * as DatabaseConnection from "../commands/models/databaseConnection";
+import { ViewNameLookup, ParameterLookup, RouteLookup } from "./lookups/routingLookup";
+import { PageResponse, CommandResponse, ApiResponse, ActiveUser, MainMenuPageResponse } from "./typeDefinitions";
+import * as ActiveUserModel from "./commands/models/activeUserModel";
+import * as EmployeeModel from "./commands/models/employeeModel";
+import * as DatabaseConnection from "./commands/models/databaseConnection";
+import * as EmployeeSignInCommand from "./commands/employees/employeeSignInCommand";
+import * as ClearActiveUser from "./commands/activeUsers/clearActiveUserCommand";
+import { error } from "console";
 
 export const start = async (req: Request, res: Response): Promise<void> => {
-	if(ActiveEmployeeExistsQuery.employeeExists){
-		return res.render(
+	if(ActiveEmployeeExistsQuery.employeeExists()){
+		res.render(
 			ViewNameLookup.SignIn
 		);
 	} else {
-		return res.redirect(ViewNameLookup.MainMenu);
+		res.redirect(ViewNameLookup.MainMenu);
 		//SHOULD REDIRECT TO EMPLOYEE DETAIL VIEW BUT DONT HAVE YET
 	}
 };
 
-export const signIn = async (req: Request, res: Response): Promise<void> => {
+export const signIn = async (req: Request, res: Response, error: any
+): Promise<void> => {
 	// TODO: Use the credentials provided in the request body (req.body)
 	//  and the "id" property of the (Express.Session)req.session variable
 	//  to sign in the user
-	var data = req.body;
-	//IF PASSWORD AND ID CORRECT, CREATE RECORD IN ACTIVEUSER TABLE
-	if(data.password===(EmployeeModel.queryByEmployeeId(data.employeeId).password)) {
-		var DatabaseConnect = DatabaseConnection.createTransaction();
-//		ActiveUserModel.init(
-//			employeeId = data.employeeId;
-//			sessionKey = (<Express.Session>req.session).id;
+	EmployeeSignInCommand.signInRequest((<Express.Session>req.session), req.body);
+	let errorMessage: (string) = "";
+	if(error.status == 404) {
+		errorMessage = error.message;
+		
+		res.render(
+			ViewNameLookup.SignIn,
+			<String>errorMessage
+		);
+	} else {
+		res.redirect(ViewNameLookup.MainMenu);
 	}
 };
 
 export const clearActiveUser = async (req: Request, res: Response): Promise<void> => {
 	// TODO: Sign out the user associated with req.session.id
+	ClearActiveUser.execute((<Express.Session>req.session).id);
+	res.send(<ApiResponse>{
+		redirectUrl: RouteLookup.SignIn
+	});
 };
